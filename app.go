38dfe86d6f9564f159a91dae0227785f045f6252
Ethan Rickaby <ethan.rickaby@sine.co>
2022-08-17 18:10:34 +0930

    Updated the namespace

--- app.go
+++ app.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"io/ioutil"
 	"log"
@@ -21,6 +22,17 @@ var (
 		"Was the last Moonraker query successful.",
 		nil, nil,
 	)
+	// klipperStatus = prometheus.NewDesc(
+	// 	prometheus.BuildFQName(namespace, "", "klipper_status"),
+	// 	"Status of member in the wan cluster. 1=Alive, 2=Leaving, 3=Left, 4=Failed.",
+	// 	[]string{"member", "dc"}, nil,
+	// )
+	extruderTemperature = prometheus.NewDesc(
+		prometheus.BuildFQName(namespace, "temperature", "celsius"),
+		"Temperature reading in degree Celsius.",
+		[]string{"printer", "name"},
+		nil,
+	)
 )
 
 type Exporter struct {
@@ -35,11 +47,12 @@ func NewExporter(moonrakerEndpoint string) *Exporter {
 
 func (e *Exporter) Describe(ch chan<- *prometheus.Desc) {
 	ch <- up
+	ch <- extruderTemperature
 }
 
 func (e *Exporter) Collect(ch chan<- prometheus.Metric) {
 	ok := e.collectStatus(ch)
-	// ok = e.collectLeaderMetric(ch) && ok
+	ok = e.collectTemperature(ch) && ok
 	if ok {
 		ch <- prometheus.MustNewConstMetric(
 			up, prometheus.GaugeValue, 1.0,
@@ -53,16 +66,63 @@ func (e *Exporter) Collect(ch chan<- prometheus.Metric) {
 
 func (e *Exporter) collectStatus(ch chan<- prometheus.Metric) bool {
 	fmt.Println("Collecting metrics from " + e.moonrakerEndpoint + "/printer/info")
-	req, err := http.NewRequest("GET", e.moonrakerEndpoint+"/printer/info", nil)
+	res, err := http.Get(e.moonrakerEndpoint + "/printer/info")
 	if err != nil {
 		fmt.Println("Failed to get Moonraker status")
 		return false
 	}
-	body, err := ioutil.ReadAll(req.Body)
-	if err != nil {
+	defer res.Body.Close()
+	body, error := ioutil.ReadAll(res.Body)
+	if error != nil {
+		fmt.Println(error)
 		return false
 	}
 	fmt.Println(string(body))
+	return true
+}
+
+type MoonrakerResponse struct {
+	Result struct {
+		Status struct {
+			Extruder struct {
+				PressureAdvance float64 `json:"pressure_advance"`
+				Target          float64 `json:"target"`
+				Power           float64 `json:"power"`
+				CanExtrude      bool    `json:"can_extrude"`
+				SmoothTime      float64 `json:"smooth_time"`
+				Temperature     float64 `json:"temperature"`
+			} `json:"extruder"`
+		} `json:"status"`
+	} `json:"result"`
+}
+
+func (e *Exporter) collectTemperature(ch chan<- prometheus.Metric) bool {
+	fmt.Println("Collecting metrics from " + e.moonrakerEndpoint + "/printer/objects/query?extruder")
+	res, err := http.Get(e.moonrakerEndpoint + "/printer/objects/query?extruder")
+	if err != nil {
+		fmt.Println("Failed to get temperature")
+		return false
+	}
+	defer res.Body.Close()
+	data, err := ioutil.ReadAll(res.Body)
+	if err != nil {
+		fmt.Println(err)
+		return false
+	}
+	fmt.Println(string(data))
+
+	var t MoonrakerResponse
+	err = json.Unmarshal(data, &t)
+	if err != nil {
+		fmt.Println(err)
+		return false
+	}
+
+	extruder := t.Result.Status.Extruder
+
+	ch <- prometheus.MustNewConstMetric(
+		extruderTemperature, prometheus.GaugeValue, extruder.Temperature, "printer-01", "extruder",
+	)
 	return true
 }
 
